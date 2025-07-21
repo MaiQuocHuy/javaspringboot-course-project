b90f5aeed9ca4d6cf537e522323bbc675092f3bf
MaiQuocHuy <maiquochuy16122003@gmail.com>
2025-07-21 11:20:08 +0700

    feat: Update project configuration and add database properties

--- springboot-app/src/main/java/project/ktc/springboot_app/SpringbootAppApplication.java
+++ springboot-app/src/main/java/project/ktc/springboot_app/SpringbootAppApplication.java
@@ -1,13 +1,23 @@
 package project.ktc.springboot_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootAppApplication {
 
+	@Value("${DB_URL:http://localhost:8080}")
+	private String dbUrl;
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAppApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootAppApplication.class, args);
+		SpringbootAppApplication app = context.getBean(SpringbootAppApplication.class);
+        System.out.println("Spring Boot Application started successfully!");
+        System.out.println("Database URL: " + app.getDbUrl());
 	}
 
+	public String getDbUrl() {
+		return dbUrl;
+	}
 }
