c0f4889b266bc09e3c5b33929722336eb895ffb3
mingttam <mingtam.713@gmail.com>
2025-08-13 13:45:17 +0700

    random password for google user

---
+++ springboot-app/src/main/java/project/ktc/springboot_app/utils/GenerateRandomPassword.java
@@ -0,0 +1,19 @@
+package project.ktc.springboot_app.utils;
+
+import java.security.SecureRandom;
+
+public class GenerateRandomPassword {
+    public static String generateRandomPassword(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        StringBuilder password = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(chars.length());
+            password.append(chars.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+
+}
